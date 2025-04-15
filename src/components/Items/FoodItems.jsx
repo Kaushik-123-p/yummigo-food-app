e18d5baa10857a1fb5ca2716c350be1dd0cf35d4
Prajapati Kaushik <kaushik@example.com>
2025-04-15 13:10:46 +0530

    Add FoodData, update FoodCard and FoodItems components to display dynamic food information

--- src/components/Items/FoodItems.jsx
+++ src/components/Items/FoodItems.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import FoodCard from '../Card/FoodCard'
+import FoodData from '../../Data/FoodData.js'
 
 const FoodItems = () => {
   return (
-    <div>
-      <FoodCard />
+    <div className='flex flex-wrap gap-10 justify-center lg:justify-start py-10'>
+      {
+        FoodData.map((food) => {
+          return (
+            <FoodCard
+              key={food.id}
+              id={food.id}
+              img={food.img}
+              name={food.name}
+              price={food.price}
+              desc={food.desc}
+              category={food.category}
+              rating={food.rating}
+            />
+          )
+        })
+      }
     </div>
   )
 }
